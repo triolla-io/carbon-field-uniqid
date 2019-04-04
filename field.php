25464f18072c9076b2795d6a2d8dd44b2b8ee2f6
Moshe Revah <zippoxer@gmail.com>
2019-04-04 12:37:23 +0300

    fix

--- field.php
+++ field.php
@@ -1,17 +1,9 @@
 <?php
-/**
- * Plugin Name: Carbon Field Number
- * Description: Number Field extension for Carbon Fields.
- * Version: 3.0.0
- * Author: htmlburger
- * Author URI: https://htmlburger.com/
- * Plugin URI: http://carbonfields.net/
- * License: GPL2
- * Requires at least: 4.0
- * Tested up to: 5.0.2
- * Text Domain: carbon-field-uniqid
- * Domain Path: /languages.
- */
-if (file_exists(__DIR__.'/vendor/autoload.php')) {
-    require __DIR__.'/vendor/autoload.php';
-}
+
+use Carbon_Field_UniqID\UniqID_Field;
+use Carbon_Fields\Carbon_Fields;
+
+define('Carbon_Field_UniqID\\DIR', __DIR__);
+Carbon_Fields::extend(UniqID_Field::class, function ($container) {
+    return new UniqID_Field($container['arguments']['type'], $container['arguments']['name'], $container['arguments']['label']);
+});
